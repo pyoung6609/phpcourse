4620755d8d0a2dd2a2acb3cb9886c88cb0bfd85a
Patrick Young <patrick.young@definition6.com>
2017-03-20 16:46:59 -0400

    Update courses logic

--- zcm/db.php
+++ zcm/db.php
@@ -1,5 +1,5 @@
 <?php
-	include 'courses.php';
+	include 'Courses.class.php';
 
  	$opts = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
  	$connection = new PDO('mysql:hostname=localhost;dbname=zcm_user_manager', 'root', '', $opts);
@@ -9,12 +9,13 @@
 		$fetch_courses_pdo = $connection->query($sql_courses, PDO::FETCH_CLASS, 'Courses');
 	    while ($row = $fetch_courses_pdo->fetchObject('Courses')) {
 	        printf('%s | %d' . PHP_EOL,
-	            $row->title, $row->category);
+	            $row->getTitle(), $row->getCategoryId());
+	        // var_dump($row);
 	    }
 	    echo '<br/>';
-
-	    $connection = null;
 	} catch(PDOException $e) {
 		echo "Error!: " . $e->getMessage() . "<br/>";
+	} finally {
+		$connection=null;
 	}
 ?>
--- zcm/libs/SMTemplate.php
+++ zcm/libs/SMTemplate.php
@@ -1,5 +1,5 @@
 <?php
-	require('db.php');
+	// require('db.php');
 	require_once('smarty/Smarty.class.php');
 	require_once('smtemplate_config.php');
 
